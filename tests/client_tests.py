3e86deb4247854ac10b4093dba85cf5fa195e66c
Levi Howa <levi@rentdynamics.com>
2023-05-11 11:24:09 -0600

    added the option for a base url and a base development url.  also updated dev url to point to rentdynamics.dev

--- tests/client_tests.py
+++ tests/client_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 
 from rentdynamics.client import Client
 
@@ -9,7 +10,19 @@ class ClientTests(unittest.TestCase):
         base_url = client.get_base_url()
         self.assertEqual(base_url, 'https://api.rentdynamics.com')
 
+    def test_when_development_is_false_with_alternate_base_url(self):
+        base_url = uuid.uuid4()[:20]
+        client = Client(base_url=base_url)
+        url = client.get_base_url()
+        self.assertEqual(url, base_url)
+
     def test_when_development_is_true_you_get_development_url(self):
         client = Client(development=True)
         base_url = client.get_base_url()
-        self.assertEqual(base_url, 'https://api-dev.rentdynamics.com')
+        self.assertEqual(base_url, 'https://api.rentdynamics.dev')
+
+    def test_when_development_is_true_with_alternate_base_url(self):
+        base_url = uuid.uuid4()[:20]
+        client = Client(base_development_url=base_url, development=True)
+        url = client.get_base_url()
+        self.assertEqual(url, base_url)
